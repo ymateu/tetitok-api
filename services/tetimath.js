252975146b1233e8be4a0cfbe71884f88de1d958
ymateu <mateusantonioofc@gmail.com>
2026-05-20 20:06:58 -0300

    fix: null

--- services/tetimath.js
+++ services/tetimath.js
@@ -1,8 +1,5 @@
 function calculateOdds(likes, dislikes) {
-    if (likes === 0) likes = 1;
-    if (dislikes === 0) dislikes = 1;
-
-    const odd = (Math.log(likes) - Math.log(dislikes)) * 10;
+    const odd = (Math.log(likes + 1) - Math.log(dislikes + 1)) * 10;
     const percentage = (likes / (likes + dislikes)) * 100;
 
     const postOdd = {
