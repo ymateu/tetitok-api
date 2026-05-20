2fdfd6f71e94ba3158298914a2f2009502856250
ymateu <mateusantonioofc@gmail.com>
2026-05-20 20:16:16 -0300

    feat: server express

---
+++ main.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const oddRouter = require('./controllers/getOdd');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/odd', oddRouter);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'API rodando',
+        instructions: 'Use POST com body JSON: { "likes": 10, "dislikes": 2 }',
+        endpoints: [
+            '/odd/percentage',
+            '/odd/odd',
+            '/odd/popularity',
+            '/odd',
+        ],
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`[+] Servidor rodando em na porta => ${port}`);
+});
