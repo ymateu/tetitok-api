11bf78a5d03daf0c483aba730dc6a95d578f9639
ymateu <mateusantonioofc@gmail.com>
2026-05-20 19:36:11 -0300

    feat: calculate odd module

---
+++ services/tetimath.js
@@ -0,0 +1,15 @@
+function calculateOdds(likes, dislikes) {
+    if (likes === 0) likes = 1;
+    if (dislikes === 0) dislikes = 1;
+
+    const odd = (Math.log(likes) - Math.log(dislikes)) * 10;
+    const percentage = (likes / (likes + dislikes)) * 100;
+
+    const postOdd = {
+        odd: odd.toFixed(2),
+        percentage: percentage.toFixed(2),
+        popularity: percentage > 60 ? 'Popular' : 'Unpopular',
+    }
+
+    return postOdd;
+}
